ea6dc7fa4ce0f8b9eb6809c6de53fd9251ab037e
Muhammad Waqas Hameed <muhammad.waqas1412@gmail.com>
2022-02-19 23:15:36 +0500

    second-commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,17 @@
-import { NgModule } from '@angular/core';
-import { RouterModule, Routes } from '@angular/router';
+import {NgModule} from '@angular/core';
+import {RouterModule, Routes} from '@angular/router';
+import {CourseListComponent} from './courses/course-list/course-list.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '', redirectTo: '/courses', pathMatch: 'full'},
+  {
+    path: 'courses', component: CourseListComponent,
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {
+}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './components/header/header.component';
+import { CoursesComponent } from './courses/courses.component';
+import { CourseListComponent } from './courses/course-list/course-list.component';
+import { CourseItemComponent } from './courses/course-list/course-item/course-item.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    CoursesComponent,
+    CourseListComponent,
+    CourseItemComponent
   ],
   imports: [
     BrowserModule,
