4bab8753358f72c7313f60e04acbbda7c3fb8014
Muhammad Waqas Hameed <muhammad.waqas1412@gmail.com>
2022-02-19 23:30:14 +0500

    # Routing error fixed.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,12 +1,10 @@
 import {NgModule} from '@angular/core';
 import {RouterModule, Routes} from '@angular/router';
 import {CourseListComponent} from './courses/course-list/course-list.component';
+import {CoursesComponent} from './courses/courses.component';
 
 const routes: Routes = [
-  {path: '', redirectTo: '/courses', pathMatch: 'full'},
-  {
-    path: 'courses', component: CourseListComponent,
-  },
+  {path: '', component: CoursesComponent},
 ];
 
 @NgModule({
