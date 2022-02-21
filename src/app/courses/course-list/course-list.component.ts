c203d5e49780e31b99c024d5851667a66cfcd82d
Muhammad Waqas Hameed <muhammad.waqas1412@gmail.com>
2022-02-22 00:18:45 +0500

    #Some UI changes

--- src/app/courses/course-list/course-list.component.ts
+++ src/app/courses/course-list/course-list.component.ts
@@ -7,9 +7,15 @@ import {Course} from '../course.model';
   styleUrls: ['./course-list.component.scss']
 })
 export class CourseListComponent implements OnInit {
-  courses: Course[] = [new Course('Angular', 'Muhammad Aftab', 'Frontend'),
+  courses: Course[] = [
+    new Course('Angular', 'Muhammad Aftab', 'Frontend'),
     new Course('Node', 'Maximilian', 'Backend'),
-    new Course('Javascript', 'Unknown', 'Blah Blah'),
+    new Course('Photoshop', 'Noah', 'Graphic Designing'),
+    new Course('Dot Net', 'Oliver', 'Backend Framework'),
+    new Course('React', 'Elijah', 'Frontend'),
+    new Course('C Sharp', 'James', 'Backend'),
+    new Course('Express', 'Jayden', 'Backend JS'),
+    new Course('Java', 'Lucas', 'Backend'),
   ];
 
 
